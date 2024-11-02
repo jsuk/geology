7f0af1051a1be35769d3fcfac738910e5dca397b
jsuk <jeremy.suk@gmail.com>
2024-11-02 21:17:57 +0900

    Limit the argument

--- geoscale.sh
+++ geoscale.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+REGEX="^[1-6]$"
+if [[ $1 =~ $REGEX ]]; then N=$1
+else N=1
+fi
 (
-paste -d, <(head -1 geological_time_chart.csv | cut -d, -f1-$1,7) <(echo duration) ;
-paste -d, <(./geochronicles.sh $1) <(./geoduration.sh $1) ) | column -s, -t
+paste -d, <(head -1 geological_time_chart.csv | cut -d, -f1-$N,7) <(echo duration) ;
+paste -d, <(./geochronicles.sh $N) <(./geoduration.sh $N) ) | column -s, -t
