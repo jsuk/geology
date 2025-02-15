bdf2922b8f5fc8649d9ab2f661ca5ca6d9c191a2
jsuk <jeremy.suk@gmail.com>
2025-02-15 15:53:15 +0900

    parametize geo time unit for plot

--- plot_duration.sh
+++ plot_duration.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-duration=$(expr $1 + 2)
-./geoscale.sh $1 | cut -d, -f$1,$duration | tail -n+2 | tail -n7 | gnuplot -e 'set ylabel "Ma";set yrange [0:24];set datafile sep ","; set term dumb 110,30;plot "<cat -" u 0:2:xtic(1) w boxes title "Duation"'
+if [ -z $1 ];
+then
+scale=1
+else
+scale=$1
+fi
+
+duration=$(expr $scale + 2)
+./geoscale.sh $scale | cut -d, -f$scale,$duration | tail -n+2 | tail -n12 | gnuplot -e 'set ylabel "Ma";set yrange [0:81];set datafile sep ","; set term dumb 110,40;plot "<cat -" u 0:($2):xtic(1) w boxes title "Duration"'
